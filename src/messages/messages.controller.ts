99d282382e835509077d0b1e912679616824e921
sahanaydn <sahanaydn1@hotmail.com>
2024-05-12 00:13:31 -0500

    added error exceptions

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message-dto';
 import { MessagesService } from './messages.service';
 
@@ -20,7 +27,12 @@ export class MessagesController {
     return this.messagesService.create(body.content);
   }
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    return this.messagesService.findOne(id);
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('message not found');
+    } else {
+      return message;
+    }
   }
 }
